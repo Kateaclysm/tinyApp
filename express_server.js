f9896b3636c0e5fa741c0c5d20d5b87f78821e7f
Katelynn <grayspence2@hotmail.com>
2023-10-24 18:42:52 -0400

    added a view directory at  urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -24,6 +24,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
